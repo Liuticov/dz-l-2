1a34a5c945d56bb23fe11936b76829f80fba8bac
Дмитрий Лютиков <ldvtransport@yandex.ru>
2023-04-17 10:00:40 +0300

    Zadaca 1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+bool CheckNumber(int number)
+{
+    if (number >= 100 && number <= 999)
+    {
+        int first = number / 10;
+        int second = first % 10;
+        Console.WriteLine("Вторая цифра в введенном числе: " + second);
+        return true;
+    }
+    else
+    { return false; }
+}
+
+Console.WriteLine("Введите трехзначное число : ");
+int a = int.Parse(Console.ReadLine());
+bool result = CheckNumber(a);
+Console.WriteLine(result);
